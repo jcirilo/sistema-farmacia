63b74dc6e461dd331c4b63e348ac86ceb5426132
Thaís Melquíades <85419606+tmelquiades@users.noreply.github.com>
2025-02-24 20:35:15 -0300

    Update usuario.py

--- classes/usuario.py
+++ classes/usuario.py
@@ -1,5 +1,5 @@
 class Usuario:
-    def __init__(self, id: int, nome: str, email: str, CPF: str):
+    def __init__(self, id: int, nome: str, email: str, cpf: str):
         self.id = id
         self.nome = nome
         self.email = email
