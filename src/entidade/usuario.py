6c2b2c0385480d6dee4bb8b4b6ff137af59b01c5
barbarahellen <barbarahellen993@gmail.com>
2025-02-24 22:49:48 -0300

    adição de usuários

--- src/entidade/usuario.py
+++ src/entidade/usuario.py
@@ -4,3 +4,13 @@ class Usuario:
         self.nome = nome
         self.email = email
         self.cpf = cpf
+
+    def __repr__(self):
+        return f"Usuário (id={self.id}, nome='{self.nome}', email='{self.email}', cpf='{self.cpf}')"
+    
+usuarios = []
+
+def adicionar_usuario(id: int, nome: str, email: str, cpf: str):
+    novo_usuario = Usuario(id, nome, email, cpf)
+    usuarios.append(novo_usuario)
+    print(f"Usuário adicionado: {novo_usuario}")
