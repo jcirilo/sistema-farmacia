de52a7aec7ab949afa08809434796acf1d524306
João V. S. Cirilo <jvscirilo12@gmail.com>
2025-03-04 19:11:22 -0300

    Modularização de control e padronização em inglês

---
+++ src/business/control/__init__.py
@@ -0,0 +1,1 @@
+from .user_control import UserControl
---
+++ src/business/control/user_control.py
@@ -0,0 +1,17 @@
+from abc import ABC
+from business.model import User
+from infra import UserPersistenceFile
+
+class UserControl(ABC):
+    
+    def add(self, user:User, persistence: UserPersistenceFile) -> User:
+        return persistence.save(user)
+
+    def list(self, name: str, persistence:UserPersistenceFile) -> User:
+        ...
+
+    def listAll(self, persistence:UserPersistenceFile) -> dict:
+        return persistence.getAll()
+
+    def delete(self, name: str, persistence:UserPersistenceFile) -> User:
+        ...
