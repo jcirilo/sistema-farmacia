efc403499ca5b3a38a859818eedc38ec411336cc
João V. S. Cirilo <jvscirilo12@gmail.com>
2025-03-09 16:21:18 -0300

    definição da classe abstrata UserPersistence

--- src/infra/user_persistence.py
+++ src/infra/user_persistence.py
@@ -10,3 +10,11 @@ class IUserPersistence(ABC):
   @abstractmethod
   def getAll(self) -> dict:
     ...
+
+  @abstractmethod
+  def delete(self, user_id: int) -> User:
+    ...
+  
+  @abstractmethod
+  def update(self, user_id: int, name: str, email: str, password: str, cpf: str) -> User:
+    ...
