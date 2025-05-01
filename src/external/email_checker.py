69d077745e124dc4fb5667fb10132f7abc533b09
bigpedrolucas <p.lucasilva@hotmail.com>
2025-04-30 22:21:32 -0300

    corrige validação de e-mail

--- src/external/email_checker.py
+++ src/external/email_checker.py
@@ -4,5 +4,5 @@ class EmailCheckerAPI:
   # simula um serviço externo de validação de email
 
   def check_email(self, email: str) -> bool:
-    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
-    return re.match(pattern, email) is not None
+    pattern = r"^(?!.*\.\.)(?!.*\.$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    return re.fullmatch(pattern, email) is not None
